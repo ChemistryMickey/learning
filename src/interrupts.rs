b96ea21f32b3a9ab000e43a4b8f5e61fba4bca69
Michael Ryan T Da Silva <michele.tdasilva@gmail.com>
2024-06-13 16:47:25 -0700

    Added page fault interrupt handler

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,10 +1,12 @@
-use crate::gdt;
-use crate::{print, println};
+#![allow(clippy::needless_return)]
+#![deny(clippy::implicit_return)]
+
+use crate::{gdt, hlt_loop, print, println};
 
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
 use spin;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
@@ -51,6 +53,7 @@ lazy_static! {
         }
         idt[InterruptIndex::Timer.into()].set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.into()].set_handler_fn(keyboard_interrupt_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         return idt;
     };
 }
@@ -68,6 +71,19 @@ extern "x86-interrupt" fn double_fault_handler(
     _error_code: u64,
 ) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {error_code:?}");
+    println!("{stack_frame:#?}");
+    hlt_loop();
 }
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
