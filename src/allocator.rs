ede9a53c16beb10151e9a12d39b87195fed6b946
Michael Ryan T Da Silva <michele.tdasilva@gmail.com>
2024-06-14 16:26:58 -0700

    Removed dummy heap allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,10 +1,9 @@
 #![allow(clippy::needless_return)]
 
-use alloc::alloc::{GlobalAlloc, Layout};
 // use bump::BumpAllocator;
-use core::ptr::null_mut;
 use linked_list::LinkedListAllocator;
 // use linked_list_allocator::LockedHeap;
+
 use x86_64::{
     structures::paging::{
         mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB,
@@ -67,16 +66,4 @@ pub fn init_heap(
     }
 
     return Ok(());
-}
-
-pub struct Dummy;
-
-unsafe impl GlobalAlloc for Dummy {
-    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
-        return null_mut();
-    }
-
-    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
-        panic!("dealloc should never be called");
-    }
 }
